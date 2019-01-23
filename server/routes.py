a95dd214fe4e8438570362de9ccd7840cac6154f
cloudscolors <andreas.mansson@live.com>
2019-01-23 13:50:30 +0100

    Added stylesheets and made an example while pending database api

--- server/routes.py
+++ server/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
-import os, getlocation
+import os
 
 app = Flask(__name__, template_folder=".")
 #path from where this file is executed.
