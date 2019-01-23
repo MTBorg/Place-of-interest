67760a7f4784884b5532f90d1fe1409ebedaafd0
Martin Terneborg <martinterneborg96@gmail.com>
2019-01-23 10:34:22 +0100

    Implemented script for creating the database

---
+++ db/setup/createDatabase.py
@@ -0,0 +1,30 @@
+import psycopg2
+from psycopg2.extensions import AsIs
+
+def createDatabase(hostName, hostPort, psqlPass, dbName, dbOwner):
+    """Creates a PostgreSQL database
+
+    This script uses (and requires) the default postgres super user and its password.
+
+    Parameters
+    ----------
+    hostName: Name/address of database host
+    hostPort: Port of the host that the database listens to 
+    psqlPass: Password of the default PostgreSQL user (postgres)
+    dbName: Name of the database to be created
+    dbOwner: Name of the PostgreSQL user to be assigned ownership of the database
+    """
+    try:
+        connection = psycopg2.connect(
+            dbname='postgres', 
+            user='postgres', 
+            host=hostName, 
+            port=hostPort, 
+            password=psqlPass) 
+        connection.autocommit=True #Don't start a transaction (database cannot be created in a transaction)
+        cursor = connection.cursor()
+        query = "CREATE DATABASE %s OWNER %s;"
+        params = (AsIs(dbName), dbOwner)
+        cursor.execute(query, params)
+    except Exception as e:
+        print("Exception creating database:", e)
