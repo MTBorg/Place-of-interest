35bbec9c6ed6b22fb49d76b90157e6e363eb9e78
Blinningjr <nicke.l@telia.com>
2019-01-23 11:19:09 +0100

    added runSetupFiles that reads json file and runs setup scripts. Also som miner modifications to setup files

--- db/setup/createDBUser.py
+++ db/setup/createDBUser.py
@@ -1,7 +1,7 @@
 import psycopg2
 from psycopg2.extensions import AsIs
 
-def createDBUser(hostName, hostPort, psqlPass, dbName, dbOwner):
+def createDBUser(hostName, hostPort, psqlPass, dbPass, dbOwner):
     """Creates a user for access to database
 
     Parameters
@@ -10,10 +10,11 @@ def createDBUser(hostName, hostPort, psqlPass, dbName, dbOwner):
     hostPort: Port of the host that the database listens to 
     psqlPass: Password of the default PostgreSQL user (postgres)
     dbName: Name of the database to be created
+    dbPass: Password for database
     dbOwner: Name of the PostgreSQL user to be assigned ownership of the database
     """
     try:
-        connection = psycopg2.connect(dbname='postgres', user='postgres', host=hostname, password=psqlPass, port=hostPort)
+        connection = psycopg2.connect(dbname='postgres', user='postgres', host=hostName, password=dbPass, port=hostPort)
         connection.autocommit=True
         cursor = connection.cursor()
         SQL = "CREATE USER %s WITH ENCRYPTED PASSWORD %s"
---
+++ db/setup/runSetupFiles.py
@@ -0,0 +1,45 @@
+import createDatabase
+import createDBUser
+import createTables
+import json
+
+
+def __runSetupFiles(filedata, connection_dict):
+    try:
+        for user in filedata["users"]:
+            createDBUser.createDBUser(connection_dict["host"], connection_dict["port"], 
+                    user["password"], 
+                    connection_dict["password"], user["username"])
+        
+        createDatabase.createDatabase(connection_dict["host"], 
+                connection_dict["port"], connection_dict["password"], 
+                connection_dict["dbname"], connection_dict["postgres"])
+        createTables.createTables(connection_dict["dbname"], connection_dict["user"], 
+                connection_dict["host"], connection_dict["password"], connection_dict["port"])
+    except Exception as e:
+        print("Error 1, Exception:", e)
+
+def __loadJasonFile(filename):
+    try:
+        with open(filename) as f:
+            filedata = json.load(f)
+    except Exception as e:
+        print("Error 2, Exception:", e)
+        return
+    return filedata
+
+def __getConnection_dict(filedata):
+    if 'connection' in filedata:
+        return filedata["connection"]
+    return {"dbname" : 'postgres', "user" :'postgres', 
+                "host" :'localhost', "password" : '123', 
+                "port" :'5432'}
+
+
+
+def run():
+    filename = 'data.jason'
+    filedata = __loadJasonFile(filename)
+    conection_dict = __getConnection_dict(filedata)
+    __runSetupFiles(filedata, conection_dict)
+
