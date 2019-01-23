dfb92d093651397d7f4d30d1900313a909fb133d
idaLu <lunida-1@student.ltu.se>
2019-01-23 10:53:40 +0100

    adds function createTables for setting up marker table in db

--- db/setup/createDBUser.py
+++ db/setup/createDBUser.py
@@ -1,13 +1,24 @@
 import psycopg2
+from psycopg2.extensions import AsIs
 
-def createDBUser():
+def createDBUser(hostName, hostPort, psqlPass, dbName, dbOwner):
+    """Creates a user for access to database
+
+    Parameters
+    ----------
+    hostName: Name/address of database host
+    hostPort: Port of the host that the database listens to 
+    psqlPass: Password of the default PostgreSQL user (postgres)
+    dbName: Name of the database to be created
+    dbOwner: Name of the PostgreSQL user to be assigned ownership of the database
+    """
     try:
-        connection = psycopg2.connect(
-                "dbname='postgres' user='postgres' host='localhost' password='123' port='5432'")
+        connection = psycopg2.connect(dbname='postgres', user='postgres', host=hostname, password=psqlPass, port=hostPort)
         connection.autocommit=True
         cursor = connection.cursor()
-        SQL = "CREATE USER admin WITH ENCRYPTED PASSWORD %s"
-        data = ("password")
+        SQL = "CREATE USER %s WITH ENCRYPTED PASSWORD %s"
+        query = "CREATE DATABASE %s OWNER %s;"
+        data = (AsIs(dbOwner), psqlPass)
         cursor.execute(SQL, data)
     except Exception as e:
         print("Exception:", e)
---
+++ db/setup/createTables.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+def createTables(dbname, username, hostname, password, portnr):
+    """Create tables in database
+
+    This script creates one table in the database for storing markers.
+
+    Parameters
+    ----------
+    dbname: Name of the database to connect to
+    username: Name of the PostgreSQL user for the database
+    hostname: Name/address of database host
+    password: Password of the default PostgreSQL user (postgres)
+    portnr: Port of the host that the database listens to
+    """
+    try:
+        connection = psycopg2.connect(dbname=dbname, user=username, host=hostname, password=password, port=portnr)
+        connection.autocommit=True
+        cursor = connection.cursor()
+        SQL = "CREATE TABLE Markers(id INTEGER NOT NULL, marker POINT, created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP, user_id VARCHAR(255) NOT NULL, PRIMARY KEY (id))"
+        cursor.execute(SQL)
+    except Exception as e:
+        print("Exception:", e)
+
