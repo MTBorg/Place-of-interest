9779d2758c68b3f6cbdc8b22b3583c1e49e31ca6
Martin Terneborg <martinterneborg96@gmail.com>
2019-01-25 08:35:43 +0100

    Added documentation for function get_markers_from_distTime

--- db/api/api.py
+++ db/api/api.py
@@ -14,7 +14,20 @@ def get_markers_from_dist(origin, radius):
     '''
     pass
 
-def get_markers_from_distTime():
+def get_markers_from_distTime(origin, radius, startTime, endTime):
+    '''Retrieves all markers within a given circle and within a given time interval
+
+    Parameters
+    ----------
+    origin - The center of the circle
+    radius - The radius of the circle
+    startTime - The start of the time interval
+    endTime - The end of the time interval
+
+    Returns
+    -------
+    A list containing all markers within the given circle and time interval
+    '''
     pass
 
 def get_markers_from_userId():
