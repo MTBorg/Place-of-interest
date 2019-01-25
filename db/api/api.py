4b67357ced33fbeafd9a3376174c298861f6bfc1
Martin Terneborg <martinterneborg96@gmail.com>
2019-01-25 08:33:39 +0100

    Added documentation for function get_markers_from_dist

--- db/api/api.py
+++ db/api/api.py
@@ -1,6 +1,17 @@
 import psycopg2
 
-def get_markers_from_dist():
+def get_markers_from_dist(origin, radius):
+    '''Retrieves all markers within a given circle
+
+    Parameters
+    ----------
+    origin - The center of the circle
+    radius - The radius of the circle
+
+    Returns
+    -------
+    A list containing all markers within the given circle 
+    '''
     pass
 
 def get_markers_from_distTime():
