f3267a8bbc64b0d3b9acf72fef32c81f62dcd287
idaLu <lunida-1@student.ltu.se>
2019-01-31 10:33:51 +0100

    replace origin input paraam in db get-functions to longitude and latitude

--- db/db.py
+++ db/db.py
@@ -54,19 +54,20 @@ class db:
         cursor = connection.cursor()
 
         query = "SELECT ST_X(ST_AsEWKT(marker)), ST_Y(ST_AsEWKT(marker)) FROM markers WHERE user_id=%s;"
-        
+
         cursor.execute(query, user_id)
         result = cursor.fetchall()
         cursor.close()
         return result
 
-    def get_markers_from_dist(self, origin, radius):
+    def get_markers_from_dist(self, lng, lat, radius):
         '''Retrieves all markers within a given circle
 
         Parameters
         ----------
         connection - Cursor for open connection
-        origin - The center of the circle
+        lng - Longitude of point in the center of the circle
+        lat - Latitude of point in the center of the circle
         radius - The radius of the circle
 
         Returns
@@ -76,8 +77,10 @@ class db:
         connection = self.__connect()
         cursor = connection.cursor()
 
-        query = "SELECT marker FROM MARKERS WHERE ST_DWithin(%s, marker, %s)"
-        data = (origin, radius)
+        coordinates = "POINT(%s %s)" % (lng, lat)
+
+        query = "SELECT ST_X(ST_AsEWKT(marker)), ST_Y(ST_AsEWKT(marker)) FROM MARKERS WHERE ST_DWithin(%s, marker, %s)"
+        data = (coordinates, radius)
 
         cursor.execute(query, data)
         result = cursor.fetchall()
@@ -85,12 +88,13 @@ class db:
         return result
         
 
-    def get_markers_from_distTime(self, origin, radius, startTime, endTime):
+    def get_markers_from_distTime(self, lng, lat, radius, startTime, endTime):
         '''Retrieves all markers within a given circle and within a given time interval
 
         Parameters
         ----------
-        origin - The center of the circle
+        lng - Longitude of point in the center of the circle
+        lat - Latitude of point in the center of the circle
         radius - The radius of the circle
         startTime - The start of the time interval
         endTime - The end of the time interval
@@ -102,8 +106,10 @@ class db:
         connection = self.__connect()
         cursor = connection.cursor()
 
-        query = "SELECT marker FROM MARKERS WHERE ST_DWithin(%s, marker, %s) AND %s <= created_at AND %s >= created_at AND"
-        data = (origin, radius, startTime, endTime)
+        coordinates = "POINT(%s %s)" % (lng, lat)
+
+        query = "SELECT ST_X(ST_AsEWKT(marker)), ST_Y(ST_AsEWKT(marker)) FROM MARKERS WHERE ST_DWithin(%s, marker, %s) AND %s <= created_at AND %s >= created_at AND"
+        data = (coordinates, radius, startTime, endTime)
 
         cursor.execute(query, data)
         result = cursor.fetchall()
--- db/testning.py
+++ db/testning.py
@@ -4,12 +4,12 @@ sys.path.insert(0,'setup')
 import setup.runSetupFiles as setup
 import db
 
-setup.run()
+#setup.run()
 db = db.db()
 db.save_marker(40,20, "1")
 api_return = db.get_markers_from_userid("1")
 print(api_return)
-#api_return1 = api.get_markers_from_dist(db_var, ST_GeomFromText("POINT(40 20)", 4326), 10000)
-#print(api_return1)
+api_return1 = db.get_markers_from_dist(40, 20, 100)
+print(api_return1)
 #api_return1 = api.get_markers_from_distTime(db_var, ST_GeomFromText("POINT(40 20)", 4326), 10000, ,)
 #print(api_return2)
