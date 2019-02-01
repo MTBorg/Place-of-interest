ad7aa575693215d45302a2819f1424803b8e1626
Martin Terneborg <martin.terneborg@xarepo.com>
2019-02-01 10:56:14 +0100

    Added a file for giving grant to a db user

---
+++ db/setup/grantDBUser.py
@@ -0,0 +1,34 @@
+import psycopg2
+from psycopg2.extensions import AsIs
+
+def grant_dbuser(db_name, dbuser_name, host_name, host_port, psql_pass):
+    '''Connects to the specified database and gives the specified
+    user grants to the markers table
+
+    Parameters
+    ----------
+    db_name: The name of the database in which the markers table is present
+    dbuser_name: The name of the user to be given the grants
+    host_name: The name of the database host
+    host_port: The port of the database
+    psql_pass: The password to the default postgres user
+    '''
+    try:
+        connection = psycopg2.connect(
+            dbname = db_name,
+            user = 'postgres',
+            host = host_name,
+            port = host_port,
+            password = psql_pass,
+        )
+        connection.autocommit = True
+        cursor = connection.cursor()
+        query = '''
+            GRANT
+            SELECT, INSERT, DELETE
+            ON markers
+            TO %s;'''
+        params = (AsIs(dbuser_name),)
+        cursor.execute(query, params)
+    except Exception as e:
+        print("Error giving user grants", e)
