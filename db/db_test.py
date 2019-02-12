f7578186c63d3d70a5af89f10bc81af0ca932950
MTBorg <martinterneborg96@gmail.com>
2019-02-12 22:40:14 +0100

    Added basic logging

--- db/db_test.py
+++ db/db_test.py
@@ -1,25 +1,31 @@
 import unittest
 import db
+import logging
+from setup import createDatabase, createDBUser, createTables
 
 class dbTest(unittest.TestCase):
+    def setUp(self):
+        logging.info("Setting up test database")
+
     def test_get_markers_from_userid(self):
-        pass
+        logging.info("Testing getting markers from user id")
     
     def test_get_markers_from_ip(self):
-        pass
+        logging.info("Testing getting markers from ip")
     
     def test_get_markers_from_userid_and_ip(self):
-        pass
+        logging.info("Testing getting markers from user id and ip")
     
     def test_get_markers_from_dist(self):
-        pass
+        logging.info("Testing getting markers from distance")
 
     def test_get_markers_from_dist_time(self):
-        pass
+        logging.info("Testing getting markers from distance and time")
 
     def test_save_marker(self):
-        pass
+        logging.info("Testing saving marker")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
     unittest.main()
