f0502aa9df5c94d4a91fbdd78571e9550fbd084f
MTBorg <martinterneborg96@gmail.com>
2019-02-12 11:34:48 +0100

    ALl exception handling in runSetupFiles.py are now done in the run function

--- db/setup/runSetupFiles.py
+++ db/setup/runSetupFiles.py
@@ -33,22 +33,21 @@ def __run_setup_files(filedata):
     logging.info("Running setup scripts")
     connection_dict = filedata["connection"]    # basic setup connection
     rw_user = filedata["user"]                  # user for read write access
-    try:
-        createDBUser.create_dbuser(connection_dict["host"], connection_dict["port"], 
-                connection_dict["password"], rw_user["username"], rw_user["password"])
-        
-        createDatabase.create_database(connection_dict["host"], 
-                connection_dict["port"], connection_dict["password"], 
-                connection_dict["dbname"], rw_user["username"])
-        
-        createTables.create_tables(connection_dict["dbname"], connection_dict["user"], 
-                connection_dict["host"], connection_dict["password"], connection_dict["port"])
 
-        grantDBUser.grant_dbuser(connection_dict["dbname"], rw_user["username"],
-                connection_dict["host"], connection_dict["port"], connection_dict["password"])
-        logging.info("Successfully ran setup scripts")
-    except Exception as e:
-        raise e
+    #Run the setup scripts
+    createDBUser.create_dbuser(connection_dict["host"], connection_dict["port"], 
+            connection_dict["password"], rw_user["username"], rw_user["password"])
+        
+    createDatabase.create_database(connection_dict["host"], 
+            connection_dict["port"], connection_dict["password"], 
+            connection_dict["dbname"], rw_user["username"])
+        
+    createTables.create_tables(connection_dict["dbname"], connection_dict["user"], 
+            connection_dict["host"], connection_dict["password"], connection_dict["port"])
+
+    grantDBUser.grant_dbuser(connection_dict["dbname"], rw_user["username"],
+            connection_dict["host"], connection_dict["port"], connection_dict["password"])
+    logging.info("Successfully ran setup scripts")
 
 
 def __load_json_file(filename):
@@ -63,32 +62,19 @@ def __load_json_file(filename):
     ----------
     A dictionary with all the data from the json file
     """
-    try:
-        dirname = os.path.dirname(__file__)
-        if (dirname == ""): #If the script is run from the same folder we don't want to prepend "/" (as it would result in searching the root)
-            filepath = filename
-        else:
-            filepath = dirname + "/" + filename
-        logging.info("Reading file %s", filepath)
-        with open(filepath) as f:
-            filedata = json.load(f)
-        return filedata 
-    except Exception as e:
-        #logging.error("Exception while reading file %s: %s", filepath, e)
-        raise Exception("Exception while reading file "+ filepath + ": " + str(e))
+    dirname = os.path.dirname(__file__)
+    if (dirname == ""): #If the script is run from the same folder we don't want to prepend "/" (as it would result in searching the root)
+        filepath = filename
+    else:
+        filepath = dirname + "/" + filename
+    logging.info("Reading file %s", filepath)
+    with open(filepath) as f:
+        filedata = json.load(f)
+    return filedata 
 
 def run():
     """Runs the script and it's functions
     """
-    try:
-        filename = 'data.json'
-
-        filedata = __load_json_file(filename)
-        __run_setup_files(filedata)
-    except:
-        raise
-
-if __name__ == "__main__":
     try:
         opts, args = getopt.getopt(sys.argv[1:], "", ["logLevel=", "logFile="])
         logging.basicConfig(format="%(asctime)s, %(levelname)s: %(message)s", level=logging.INFO)
@@ -116,17 +102,26 @@ if __name__ == "__main__":
                     logging.info("Disabling logging")
                     logging.disable(logging.CRITICAL)
                 else:
-                    raise Exception("Invalid logLevel argument " + arg)
+                    raise ValueError("Invalid logLevel argument " + arg)
             elif opt=="--logFile":
                 flog_handler = logging.FileHandler(arg)
                 flog_handler.setLevel(logging.INFO)
                 flog_format = logging.Formatter("%(asctime)s, %(levelname)s: %(message)s")
                 flog_handler.setFormatter(flog_format)
                 logging.getLogger().addHandler(flog_handler)
-        run()
-    except getopt.GetoptError as e:
-        print(e)
+
+        filename = 'data.json'
+
+        filedata = __load_json_file(filename)
+        __run_setup_files(filedata)
+    except getopt.GetoptError:
         print("Usage:", os.path.basename(__file__), "--logLevel <logLevel> --logFile <logFile>")
-    except Exception as e:
-        logging.error(e)
-        raise e
+    except OSError:
+        logging.exception("OSError")
+    except ValueError as e:
+        logging.error("ValueError: %s", e)
+    except:
+        logging.exception("Unknown exception")
+
+if __name__ == "__main__":
+    run()
