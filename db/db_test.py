e98f1e763e0881e2f575bcdb2c13d485ba363e27
Martin Terneborg <martinterneborg96@gmail.com>
2019-02-12 22:19:55 +0100

    Added placeholder test for all db api functions

---
+++ db/db_test.py
@@ -0,0 +1,25 @@
+import unittest
+import db
+
+class dbTest(unittest.TestCase):
+    def test_get_markers_from_userid(self):
+        pass
+    
+    def test_get_markers_from_ip(self):
+        pass
+    
+    def test_get_markers_from_userid_and_ip(self):
+        pass
+    
+    def test_get_markers_from_dist(self):
+        pass
+
+    def test_get_markers_from_dist_time(self):
+        pass
+
+    def test_save_marker(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
